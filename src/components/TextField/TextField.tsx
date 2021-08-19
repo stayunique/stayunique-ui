f07309020b425d3ee85471ecf149fbd22458a569
Josh Fradley <josh.fradley@gmail.com>
2021-08-19 17:52:13 +0200

    Add multiline option

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,12 +8,13 @@ export type TextFieldType = {
     value?: string | undefined;
     onChange: (value: any) => void;
     fullWidth?: boolean;
+    multiline?: boolean;
 };
 
-function TextField({ className, label, placeholder, value, onChange, fullWidth }: TextFieldType) {
+function TextField({ className, label, placeholder, value, onChange, fullWidth, multiline }: TextFieldType) {
     const change = (event: any) => onChange(event.target.value);
 
-    return <MUITextField className={className} label={label} placeholder={placeholder} value={value} onChange={change} fullWidth={fullWidth} />;
+    return <MUITextField className={className} label={label} placeholder={placeholder} value={value} onChange={change} fullWidth={fullWidth} multiline={multiline} />;
 }
 
 export default TextField;
